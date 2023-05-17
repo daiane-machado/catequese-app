bcf5342735fb57177a00f60faa7ee6c4857f8e11
DaiMM <dai.mmachado@gmail.com>
2023-05-16 23:10:19 -0300

    funcionalidade filter

--- src/provider/context.tsx
+++ src/provider/context.tsx
@@ -5,21 +5,27 @@ interface IGlobalContextProps {
   setCatechisms: (catechism: any) => void;
   updateDatas: boolean;
   setUpdateDatas: (update: boolean) =>void;
+  filterDatas: any;
+  setFilterDatas: (data: any) => void;
 }
 
 export const GlobalContext = createContext<IGlobalContextProps>({
   catechisms: {},
   setCatechisms: () => { },
   updateDatas: false,
-  setUpdateDatas: () => { }
+  setUpdateDatas: () => { },
+  filterDatas: '',
+  setFilterDatas: () =>''
 })
 
 export const GlobalContextProvider = ({children}: any) => {
   const listCatechism = children.props.data
   const valueData = false
+  const filter = ''
   
   const [listCatechisms, setListCatechisms] = useState(listCatechism)
   const [updateDatas, setUpdateDatas] = useState(valueData)
+  const [filterDatas, setFilterDatas] = useState(filter)
  
   return (
     <GlobalContext.Provider
@@ -27,7 +33,9 @@ export const GlobalContextProvider = ({children}: any) => {
         catechisms: listCatechisms,
         setCatechisms: setListCatechisms,
         updateDatas: valueData,
-        setUpdateDatas: setUpdateDatas
+        setUpdateDatas: setUpdateDatas,
+        filterDatas: filterDatas,
+        setFilterDatas: setFilterDatas
       }}>
       {children}
     </GlobalContext.Provider>
